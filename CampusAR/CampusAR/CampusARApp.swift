11f6b8d8b7a8e07c6bef156046779dd6fe335991
Winnie Yong <winnie@Winnies-MacBook-Air.local>
2024-09-21 15:03:56 -0400

    Updated VSCode configuration for Swift and UIKit

---
+++ CampusAR/CampusAR/CampusARApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusARApp.swift
+//  CampusAR
+//
+//  Created by Winnie Yong on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CampusARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CampusAR/CampusARTests/CampusARTests.swift
@@ -0,0 +1,16 @@
+//
+//  CampusARTests.swift
+//  CampusARTests
+//
+//  Created by Winnie Yong on 9/21/24.
+//
+
+import Testing
+
+struct CampusARTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
